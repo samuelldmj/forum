48d5a39cafa9f6aa5a870e52362b0747c6eeec53
Samuel <samuelldmj5@gmail.com>
2024-05-30 11:01:11 +0100

    config files created

---
+++ config/dbconn.php
@@ -0,0 +1,25 @@
+<?php 
+
+//host
+define('HOST', 'localhost');
+
+//pass
+define('PASS', '');
+
+//user
+define('USER', 'root');
+
+//dbname
+define('DBNAME', '');
+
+
+//CONNECTION
+$conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME.";",USER ,PASS);
+
+//checking if the connection was succesful
+if (!$conn) {
+echo "Connection Failed";
+}
+
+
+?>

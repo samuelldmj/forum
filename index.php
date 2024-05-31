c7d6b1f23707ca6cb373a0f6c9735b80b9a7d38a
Samuel <samuelldmj5@gmail.com>
2024-05-31 12:54:16 +0100

    dispalyed the post in the index page

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
 <?php require 'include/header.php'; ?>
 <?php require 'config/dbconn.php'; ?>
 
+<?php 
+
+ //displaying the posts dynamically
+
+ $allposts = $conn->query("SELECT * FROM post ");
+ $allposts->execute();
+ $posts = $allposts->fetchAll(PDO::FETCH_OBJ);
+
+?>
+
 
 <!-- Main content -->
 <div style="margin-top: 43px;" class="col-lg-9 mb-3">
-  <!-- <div class="row text-left mb-5">
-              
-              
-            </div> -->
+  <!-- <div class="row text-left mb-5">           
+      </div> -->
   <!-- End of post 1 -->
+  <?php foreach($posts as $post): ?>
   <div
     class="mt-5 card row-hover pos-relative py-3 px-3 mb-3 border-warning border-top-0 border-right-0 border-bottom-0 rounded-0">
     <div class="row align-items-center">
       <div class="col-md-8 mb-3 mb-sm-0">
         <h5>
-          <a href="single.php?=<?php echo 1; ?>" class="text-primary">Drupal 8 quick starter guide</a>
+          <a href="single.php?id=1" class="text-primary"><?php  echo $post->title; ?></a>
         </h5>
-        <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">20 minutes</a> <span
-            class="op-6">ago by</span> <a class="text-black" href="#">KenyeW</a></p>
-        <div class="text-sm op-5"> <a class="text-black mr-2" href="#">Programming</a></div>
+        <p class="text-sm"><span class="op-6">Posted</span> <?php echo  $post->body; ?> <a class="text-black"
+            href="#"><?php echo  $post->created_at; ?></a>
+          <span class="op-6">ago by</span> <a class="text-black" href="#"><?php echo  $post->author; ?></a></p>
+        <div class="text-sm op-5"> <a class="text-black mr-2" href="#"><?php  echo  $post->category; ?></a></div>
       </div>
       <div class="col-md-4 op-7">
         <div class="row text-center op-7">
@@ -28,70 +38,8 @@
       </div>
     </div>
   </div>
+  <?php endforeach; ?>
   <!-- /End of post 1 -->
-  <!-- End of post 2 -->
-  <div
-    class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
-    <div class="row align-items-center">
-      <div class="col-md-8 mb-3 mb-sm-0">
-        <h5>
-          <a href="#" class="text-primary">HELP! My Windows XP machine is down</a>
-        </h5>
-        <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">54 minutes</a> <span
-            class="op-6">ago by</span> <a class="text-black" href="#">DanielD</a></p>
-        <div class="text-sm op-5"> <a class="text-black mr-2" href="#">Programming</a></div>
-      </div>
-      <div class="col-md-4 op-7">
-        <div class="row text-center op-7">
-          <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">122
-              Replys</span> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /End of post 2 -->
-  <!-- End of post 3 -->
-  <div
-    class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
-    <div class="row align-items-center">
-      <div class="col-md-8 mb-3 mb-sm-0">
-        <h5>
-          <a href="#" class="text-primary">Bootstrap 4 development in record time with AppStrap Bootstrap 4 Theme</a>
-        </h5>
-        <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">32 minutes</a> <span
-            class="op-6">ago by</span> <a class="text-black" href="#">AppStrapMaster</a></p>
-        <div class="text-sm op-5"> <a class="text-black mr-2" href="#">Programming</a></div>
-      </div>
-      <div class="col-md-4 op-7">
-        <div class="row text-center op-7">
-          <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">122
-              Replys</span> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /End of post 3 -->
-  <!-- End of post 4 -->
-  <div
-    class="card row-hover pos-relative py-3 px-3 mb-3 border-warning border-top-0 border-right-0 border-bottom-0 rounded-0">
-    <div class="row align-items-center">
-      <div class="col-md-8 mb-3 mb-sm-0">
-        <h5>
-          <a href="#" class="text-primary">Bootstrap 4 development in record time with AppStrap Bootstrap 4 Theme</a>
-        </h5>
-        <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">29 minutes</a> <span
-            class="op-6">ago by</span> <a class="text-black" href="#">Themelize.me</a></p>
-        <div class="text-sm op-5"> <a class="text-black mr-2" href="#">Programming</a></div>
-      </div>
-      <div class="col-md-4 op-7">
-        <div class="row text-center op-7">
-          <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">122
-              Replys</span> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /End of post 4 -->
 
 </div>
 
--- single.php
+++ single.php
@@ -27,6 +27,12 @@
     
   }
 
+  //displaying the replies dynamically
+
+  $allReplies = $conn->query("SELECT * FROM replies where post_id = 1 ");
+  $allReplies->execute();
+  $replies = $allReplies->fetchAll(PDO::FETCH_OBJ);
+
 
 
 
@@ -69,22 +75,21 @@
         <h5>
           <a href="#" class="text-primary">Write Comment</a>
         </h5>
-        <form method="POST" action="single.php">
+        <form method="POST" action="single.php?id=1">
           <div class="form-group">
             <label for="exampleFormControlInput1">Author Name</label>
-            <input type="text" name="author_name" class="form-control" id="exampleFormControlInput1"
-              placeholder="author name">
+            <input type="text" name="author_name" class="form-control" id="exampleFormControlInput1">
           </div>
 
           <div class="form-group">
             <label for="exampleFormControlTextarea1">Reply</label>
             <textarea name="reply" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-<div class="form-group">
-  <input type="hidden" name="post_id" class="form-control">
-</div>
+          </div>
 
-<button name="submit" type="submit" class="mt-4 btn btn-primary w-25">add reply</button>
+          <div class="form-group">
+            <input type="hidden" name="post_id" value="<?php echo 1;?>" class="form-control">
+            </div>
+          <button name="submit" type="submit" class="mt-4 btn btn-primary w-25">reply</button>
         </form>
       </div>
 
@@ -92,37 +97,23 @@
   </div>
 
   <!-- Replies -->
+  <?php foreach($replies as $rep): ?>
   <div style="margin-left: 40px;"
     class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
     <div class="row align-items-center">
       <div class="col-md-12 mb-3 mb-sm-0">
         <h5>
-          <a href="#" class="text-primary">Mohamed Hassan</a>
+          <a href="#" class="text-primary"><?php echo $rep->author_name; ?></a>
         </h5>
         <p>
-          Appropriately cultivate principle-centered infrastructures via world-class niches
+          <?php echo $rep->reply; ?>
         </p>
-        <p class="text-sm"><span class="op-6">Commented</span> <a class="text-black" href="#">20 minutes</a> ago</p>
+        <p class="text-sm"><span class="op-6">Commented</span> <a class="text-black"
+            href="#"><?php echo $rep->created_at; ?></a> </p>
       </div>
-
     </div>
   </div>
-
-  <div style="margin-left: 40px;"
-    class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
-    <div class="row align-items-center">
-      <div class="col-md-12 mb-3 mb-sm-0">
-        <h5>
-          <a href="#" class="text-primary">Mohamed Hassan</a>
-        </h5>
-        <p>
-          Appropriately cultivate principle-centered infrastructures via world-class niches
-        </p>
-        <p class="text-sm"><span class="op-6">Commented</span> <a class="text-black" href="#">20 minutes</a> ago</p>
-      </div>
-
-    </div>
-  </div>
+  <?php endforeach; ?>
 
 </div>
 
